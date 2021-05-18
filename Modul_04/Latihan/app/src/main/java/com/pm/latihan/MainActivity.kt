5640b0d72deba767f760d64cd9db5fba6f56ea8e
MuhammadMakhfud <makhfud1107@gmail.com>
2021-05-18 22:43:19 +0800

    4

---
+++ Modul_04/Latihan/app/src/main/java/com/pm/latihan/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pm.latihan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener {
+            Toast.makeText (this@MainActivity,"Berhasil", Toast.LENGTH_LONG).show()
+        }
+    }
+}
